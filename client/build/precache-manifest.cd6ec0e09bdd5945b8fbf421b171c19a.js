7d2a7d7122a0d9d7f74b484debbe408ddebfe2d9
shoaiburrehman <shoaiburrehman249@gmail.com>
2020-09-19 17:11:26 +0500

    first commit

---
+++ client/build/precache-manifest.cd6ec0e09bdd5945b8fbf421b171c19a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dbe4b686634dc371fad51336d59a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b4eee0e8005899b7a7f",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "7d97565a6f0d83efdc6c",
+    "url": "/static/js/2.0f2b02c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f2b02c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4eee0e8005899b7a7f",
+    "url": "/static/js/main.9c9bd334.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
